853873c8ff86331e34b4fb279032bceb9e6dc537
Tabasko <s.rueber@gmx.net>
2023-03-31 16:56:38 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,8 @@ import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
 import { HttpClientModule } from '@angular/common/http';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,8 @@ import { HttpClientModule } from '@angular/common/http';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,7 +3,6 @@ import { Observable } from 'rxjs';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 import { MessageService } from '../message.service';
-import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -11,7 +10,7 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes$: Observable<Hero[]> |  undefined = undefined;
+  heroes: Hero[] = [];
 
   constructor(private heroService: HeroService, private messageService: MessageService) {
   }
@@ -21,7 +20,23 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes$ = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+    .subscribe((heroes: Hero[]) => this.heroes = heroes);
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe((hero: any) => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((h: Hero) => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
 
 }
