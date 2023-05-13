d626706daa429b8b7a594a5e4820c364d3306855
Tabasko <wastelinio@Sebastians-Air.fritz.box>
2023-05-13 15:52:52 +0200

    +polling test and excel

--- src/app/feature/feature-heroes/dashboard/dashboard.component.ts
+++ src/app/feature/feature-heroes/dashboard/dashboard.component.ts
@@ -1,22 +1,48 @@
-import { Component, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Observable, Subscription, interval, startWith, switchMap, timer } from "rxjs";
 import { Hero } from "../model/hero";
 import { HeroesFacade } from "../store/store.facade";
+import { HeroService } from "../service/hero.service";
+import { ExcelService } from "../excel.service";
 
 @Component({
   selector: "app-dashboard",
   templateUrl: "./dashboard.component.html",
   styleUrls: ["./dashboard.component.css"],
 })
-export class DashboardComponent implements OnInit {
-  heroes$: Observable<Hero[]>;
+export class DashboardComponent implements OnInit, OnDestroy{
+  heroes: Hero[] = [];
+  timeInterval?: Subscription;
+  errorMessage?: string;
 
-  constructor(private heroesFacade: HeroesFacade) {
-    this.heroes$ = heroesFacade.heroes$;
+  constructor(private heroesFacade: HeroesFacade, private heroService: HeroService, private excelService: ExcelService) {
+    //this.heroes$ = heroesFacade.heroes$;
+  }
+  ngOnDestroy(): void {
+    this.timeInterval?.unsubscribe();
+  }
+
+  exportexcel(){
+
   }
 
   ngOnInit(): void {
     this.getHeroes();
+
+    this.timeInterval = timer(5000, 10000)
+    .pipe(
+      startWith(0),
+      switchMap(()=> this.heroService.getHeroes())
+    ).subscribe({
+      next: (data) => {
+        console.log(this.heroes);
+        this.heroes?.push(data[0]);
+      }
+        ,
+      error: error => this.errorMessage= error,
+      complete: () => {}}
+    );
+
   }
 
   getHeroes(): void {
